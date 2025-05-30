387ea3b6ef72ba4eb2dc72853041ed15afa9e856
Marcelloax <marcellomachado2014@gmail.com>
2025-05-30 19:58:27 -0300

    atualizado 30/05

--- beecrowd/atividades9.js
+++ beecrowd/atividades9.js
@@ -4,7 +4,7 @@ var lines = input.split('\n');
 var Nome = lines[0];
 var salario = parseFloat(lines[1]);
 var vendas = parseFloat(lines[2]);
-var final = (salario * 0.15)+salario;
+var final = (vendas * 0.15)+salario;
 
 
-console.log("o total a receber no final do mês e " + final.toFixed(2));
+console.log("TOTAL = R$ " + final.toFixed(2));
